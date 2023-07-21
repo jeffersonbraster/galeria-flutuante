406840839458f0c2ff8b3803b289ea994b544053
Jefferson <jeffersonbraster@gmail.com>
2023-07-21 04:08:35 -0300

    enviando projeto

---
+++ src/data.js
@@ -0,0 +1,11 @@
+import Foto1 from "@/assets/foto1.jpg";
+import Foto2 from "@/assets/foto2.jpg";
+import Foto3 from "@/assets/foto3.jpg";
+import Foto4 from "@/assets/foto4.jpg";
+import Foto5 from "@/assets/foto5.jpg";
+import Foto6 from "@/assets/foto6.jpg";
+import Foto7 from "@/assets/foto7.jpg";
+import Foto8 from "@/assets/foto8.jpg";
+import Foto9 from "@/assets/foto9.jpg";
+
+export { Foto1, Foto2, Foto3, Foto4, Foto5, Foto6, Foto7, Foto8, Foto9 };
